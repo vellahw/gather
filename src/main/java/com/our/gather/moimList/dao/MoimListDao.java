931f4d129f026f22b22298e73eea310a047f6a18
Vella <112395421+vellahw@users.noreply.github.com>
2023-12-19 00:28:48 +0900

    231219 - test

--- src/main/java/com/our/gather/moimList/dao/MoimListDao.java
+++ src/main/java/com/our/gather/moimList/dao/MoimListDao.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import com.our.gather.common.dao.AbstractDao;
 
 @Repository("moimListDao")
-public class MoimListDao extends AbstractDao {
+public class MoimListDao {
 
 }
