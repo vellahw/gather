df25b51cf4a62fcb5038d854f82c7c98d26c90d3
Vella <112395421+vellahw@users.noreply.github.com>
2023-12-26 15:15:21 +0900

    231226 - 메인페이지 카테고리 스타일 적용

--- src/main/webapp/WEB-INF/views/mainPage/mainPage.jsp
+++ src/main/webapp/WEB-INF/views/mainPage/mainPage.jsp
@@ -1,9 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<script defer="defer" src="/resources/js/mainPage/mainPage.js"></script>
+<link rel="stylesheet" href="/resources/css/mainPage/mainPage.css">
+<script src="/resources/js/mainPage/mainPage.js"></script>
 <div class="bannerContainer">
   <img src="/resources/img/banner/banner.png">
 </div>
-<div id="categoryContainer">
+
+<div class="categoryContainer" id="categoryContainer">
+  <ul class="categoryList">
+    <li class="categoryItem">
+      <a href="#" class="categoryLink">전체</a>
+    </li>
+    <li class="categoryItem">
+      <a href="#" class="categoryLink">
+        <img src="/resources/img/icon/category/sp.png" class="categoryIcon" alt="카테고리 이미지">
+        문화·예술
+      </a>
+    </li>
+    <li class="categoryItem">
+      <a href="#" class="categoryLink">운동·액티비티</a>
+    </li>
+  </ul>
 </div>
 </body>
--- src/main/webapp/resources/js/mainPage/mainPage.js
+++ src/main/webapp/resources/js/mainPage/mainPage.js
@@ -2,25 +2,25 @@
  * 231224 장한원
  */
 
-document.addEventListener("DOMContentLoaded", function(){
-  const categoryContainer = document.getElementById('categoryContainer');
+// document.addEventListener("DOMContentLoaded", function(){
+//   const categoryContainer = document.getElementById('categoryContainer');
   
-  //새로 생성되는 카테고리 li
-  let categoryItem = document.createElement('li');
-  categoryItem.class = 'categoryItem';
-  categoryItem.style = 'color:red;';
+//   //새로 생성되는 카테고리 li
+//   let categoryItem = document.createElement('li');
+//   categoryItem.className = 'categoryItem';
+//   categoryItem.style = 'color:red;';
   
-  //새로 생성되는 카테고리 a 태그 
-  let categoryLink = document.createElement('a');
-  categoryLink.class = 'categoryLink';
-  categoryLink.textContent = '전체';
-  // newDeleteFileInputBtn.dataset.fileName = fileName;
+//   //새로 생성되는 카테고리 a 태그 
+//   let categoryLink = document.createElement('a');
+//   categoryLink.className = 'categoryLink';
+//   categoryLink.textContent = '전체';
+//   // newDeleteFileInputBtn.dataset.fileName = fileName;
   
-  let categoryList = document.createElement('ul');
-  categoryList.class = 'categoryList';
-  categoryList.appendChild(categoryItem);
-  categoryList.appendChild(categoryLink);
+//   let categoryList = document.createElement('ul');
+//   categoryList.className = 'categoryList';
+//   categoryList.appendChild(categoryItem);
+//   categoryList.appendChild(categoryLink);
   
-  //추가해주기
-  categoryContainer.appendChild(categoryList);
-})
+//   //추가해주기
+//   categoryContainer.appendChild(categoryList);
+// })
