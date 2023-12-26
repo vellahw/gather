ef8675728ad5e8a91e70baa43a28062ed3950733
Vella <112395421+vellahw@users.noreply.github.com>
2023-12-26 15:14:17 +0900

    231226 - 안쓰는거 삭제

--- src/main/java/com/our/gather/HomeController.java
+++ src/main/java/com/our/gather/HomeController.java
@@ -1,16 +1,14 @@
 package com.our.gather;
 
-import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home() {
 
 		return "home";
 	}
