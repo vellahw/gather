8c37f9b38085b79138ca12791a43d30207d4762e
Vella <112395421+vellahw@users.noreply.github.com>
2024-01-09 18:32:51 +0900

    240109 - 오타 수정

--- src/main/webapp/resources/js/common/category.js
+++ src/main/webapp/resources/js/common/category.js
@@ -1,24 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
-  
-  /**
-  * 231224 장한원
-  * 동적 슬라이드 버튼 생성
-  */
-  const contentsContainer = document.querySelector('.slideContentsWrap');
-  const hiddenArrowBtn = document.querySelectorAll('.arrowBtn');
-  
-  contentsContainer.addEventListener('mouseenter', () => {
-    for (let i = 0; i < hiddenArrowBtn.length; i++) {
-      hiddenArrowBtn[i].classList.add('btnHover');
-    }
-  })
-  
-  contentsContainer.addEventListener('mouseleave', () => {
-    for (let i = 0; i < hiddenArrowBtn.length; i++) {
-      hiddenArrowBtn[i].classList.remove('btnHover');
-    }
-  })
-
   /**
   * 240103 장한원
   * 카테고리바 hover시 하위 카테고리 보여짐
