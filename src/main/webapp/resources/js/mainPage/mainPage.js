f4f2310d341cecae608d58d37f38736815a107cb
Vella <112395421+vellahw@users.noreply.github.com>
2024-01-16 00:12:55 +0900

    240116 - 슬라이드 완성! 버튼만..

--- src/main/webapp/resources/js/mainPage/mainPage.js
+++ src/main/webapp/resources/js/mainPage/mainPage.js
@@ -7,8 +7,6 @@ function contentsSlider(){
   const slideContainer = document.querySelectorAll('.slideContainer'); // 리스트를 감싸는 부모
   
   slideContainer.forEach((slideContainer) => {
-    let currentIndex = 0; // 현재 인덱스
-    let translate = 0;  // 슬라이드 이동값
 
     const slideList = slideContainer.querySelectorAll('.slideList'); // 컨텐츠를 감싸는 리스트
     
@@ -17,153 +15,66 @@ function contentsSlider(){
 
       const slideContents = slideList.querySelectorAll('.slideContents'); // 컨텐츠
       let slideContentCount = slideContents.length;
-      let count = slideContentCount - 4;
-
-      if(count > 4) {
-        slideList.dataset.curIdx = 2;
-      } else if(count > 0){
-        slideList.dataset.curIdx = 1;
-      } else {
-        slideList.dataset.curIdx = 0;
-      }
 
       // 컨텐츠가 4개 초과일 때
       if(slideContentCount > 4) {
 
-        slideContents.forEach((slideContents, i)=>{
-          // const targetContentsIndex = slideContents.getAttribute('data-index');
-        })
+        const btn = slideContainer.querySelectorAll('.arrowBtn'); // 화살표 버튼
 
-        const rightBtn = slideContainer.querySelectorAll('#rigthBtn');
-        const leftBtn = slideContainer.querySelectorAll('#leftBtn');
-
-        rightBtn.forEach(rightBtn => {
-
-           // 마우스 올리면 오른쪽 버튼 등장
+        let currentIdx = 0; // 슬라이드 현재 번호
+        let translate = 0; // 슬라이드 위치 값
+        
+        btn.forEach((btn) => {
+          
+          const targetB = slideContainer.querySelector('#rigthBtn');
+          const targetleftB = slideContainer.querySelector('#leftBtn');
+          
           slideContainer.addEventListener('mouseenter', () => {
-            rightBtn.classList.add('_hover');
+            if(currentIdx > 0) {
+              targetleftB.classList.add('_hover');
+            }
+              targetB.classList.add('_hover');
           })
-
+          
           // 마우스 떠나면 버튼 사라짐
           slideContainer.addEventListener('mouseleave', () => {
-            rightBtn.classList.remove('_hover');
+            targetB.classList.remove('_hover');
           })
-
-          // 클릭 이벤트 등록
-          rightBtn.addEventListener('click', nextSlide)
-
-        })
-
-        function nextSlide() {
-          const slideCurrentIdx = slideList.getAttribute('data-cur-idx');
-          currentIndex++;
           
-          if(currentIndex <= slideCurrentIdx) { // currentIndex <= 3
-            translate = 1056 * currentIndex;
-            slideList.style.transform = `translateX(-${translate}px)`;
-            slideList.style.transition = `all 400ms ease`
-          }
-
-        }
+          /* 이벤트 등록 */
+          btn.addEventListener('click', moveSlide);
         
-        // if(currentIndex != 0){
-        //   leftBtn.forEach(leftBtn =>{
-        //     // 마우스 올리면  쪽 버튼 등장
-        //     slideContainer.addEventListener('mouseenter', () => {
-        //       leftBtn.classList.add('_hover');
-        //     })
-
-        //   })
-        // }
-
-        
-        // const btn = slideContainer.querySelectorAll('.arrowBtn'); // 화살표 버튼
-        
-        // /* 버튼 제어 */
-        // btn.forEach((btn) => {
-          
-        //   // 마우스 올리면 버튼 등장
-        //   slideContainer.addEventListener('mouseenter', () => {
-        //       btn.classList.add('_hover');
-        //   })
-  
-        //   // 마우스 떠나면 버튼 사라짐
-        //   slideContainer.addEventListener('mouseleave', () => {
-        //       btn.classList.remove('_hover');
-        //   })
-
-        //   // 클릭 이벤트 등록
-        //   btn.addEventListener('click', moveSlide); 
-          
-        //   let currentIdx = 0; // 현재 인덱스
-        //   let translate = 0;  // 슬라이드 이동값
-
-        //   /* 슬라이드 실행 */
-        //   function move(curIdx) {
-
-        //     currentIdx += (-1 * curIdx); // c = c + (-1 * -1)
-        //     translate += 1056 * curIdx; // tl = tl + 1056 * -1
+          /* 버튼 클릭 이벤트 콜백 함수 */
+          function moveSlide(event) {
+            event.preventDefault();
             
-        //     slideList.style.transform = `translateX(${translate}px * ${currentIdx})`;
-        //     slideList.style.transition = `all .4ms ease`
+            // 오른쪽 버튼 클릭시
+            if(event.target.classList.contains("right")) {
+              if (currentIdx !== 2) {
+                currentIdx += 1;
+                translate = 1056 * currentIdx;
+                slideList.style.transform = `translateX(-${translate}px)`;
+                slideList.style.transition = `all 400ms ease`
+              }
 
-        //   }
+              // 마지막 슬라이드라면 오른쪽 버튼 안 보이게함
+              // if(currentIdx === 2) {
+              //   event.target.style.opacity = '0';
+              //   event.target.style.zIndex = '1';
+              // }
+
+
+            // 왼쪽 버튼 클릭시
+            } else if(event.target.classList.contains("left")) {
+                if (currentIdx !== 0) {
+                  currentIdx -= 1; // 1 0
+                  translate = 1056 * -currentIdx;
+                  slideList.style.transform = `translateX(${translate}px)`;
+                }
+              }
+          }
           
-        //   /* 클릭 이벤트 함수 */
-        //   function moveSlide(event) {
-
-        //     event.preventDefault();
-
-        //     if (event.target.classList.contains("left")) {
-              
-        //       currentIdx = -1;
-        //       translate = 1056 * (currentIdx * 2);
-        //       slideList.style.transform = `translateX(${translate}px)`; // 1056 * -2
-        //       slideList.style.transition = `all 400ms ease`
-
-        //       // if(currentIdx == -1) {
-        //       //   setTimeout(() => {
-        //       //     slideList.style.transition = 'none';
-        //       //     currentIdx = 0;
-        //       //     translate = -1056;
-        //       //     slideList.style.transform = `translateX(${translate}px)`;
-        //       //   }, 400);
-        //       // }
-
-        //       // move(-1);
-        //       // if (currentIdx === 2)
-        //       //   setTimeout(() => {
-        //       //     slideList.style.transition = 'none';
-        //       //     currentIdx = 1;
-        //       //     translate = -1056;
-        //       //     slideList.style.transform = `translateX(${translate}px)`;
-        //       //   }, speedTime);
-                
-        //     } else {
-
-        //       currentIdx = 0;
-        //       translate = 1056 * currentIdx;
-        //       slideList.style.transform = `translateX(${translate}px)`; // 1056 * 2
-        //       slideList.style.transition = `all 400ms ease`
-             
-        //       //move(1);
-        //       // if (currentIdx === 0) {
-          
-        //       //   setTimeout(() => {
-    
-        //       //     slideList.style.transition = 'none';
-        //       //     currentIdx = 2;
-        //       //     translate = -(1056 * currentIdx);
-        //       //       slideList.style.transform = `translateX(${translate}px)`;
-    
-        //       //   }, speedTime);
-        //       // }
-
-
-        //     }
-        //   }
-
-        // }) // END btn.forEach
+        }) // END btn.forEach
 
       } // END  if(slideContentCount > 4)
 
