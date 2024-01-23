fcb6482a90cf00234873853828255e6b76d68e13
Vella <112395421+vellahw@users.noreply.github.com>
2024-01-23 19:00:02 +0900

    240123 -- 리스트 페이지 css 적용

--- src/main/webapp/resources/js/listPage/listPage.js
+++ src/main/webapp/resources/js/listPage/listPage.js
@@ -1,9 +1,24 @@
 window.onload = function () {
 
-    const scrollPosition = localStorage.getItem('scrollPosition') || 0;
-    window.scrollTo(0, scrollPosition);
-
-};
+  const scrollPosition = localStorage.getItem('scrollPosition') || 0;
+  window.scrollTo(0, scrollPosition);
+    
+  // 마우스오버하면 모임 이미지 확대 효과
+  const eachWrap = document.querySelectorAll('.eachWrap');
+  eachWrap.forEach((eachCard) => {
+      
+    const thumnailImg = eachCard.querySelector('.thumnail');
+    
+    eachCard.addEventListener('mouseover', function() {
+      thumnailImg.style.transform = 'scale(1.05)';
+    });
+  
+    eachCard.addEventListener('mouseout', function() {
+      thumnailImg.style.transform = 'scale(1)';
+    });
+    
+  })
+}
 
 
 // 페이지를 떠날 때 스크롤 위치를 저장합니다.
