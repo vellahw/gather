6bf219dba17f50c6de9848861672d5ee9c89157e
Vella <112395421+vellahw@users.noreply.github.com>
2024-02-08 02:30:01 +0900

    240208 -- 지역 데이터 http 응답으로 받아봄..

--- src/main/webapp/resources/js/loginPage/join.js
+++ src/main/webapp/resources/js/loginPage/join.js
@@ -79,6 +79,24 @@ function btnOnclick() {
  * 선호 지역 데이터 관리
  */
 function controlRegiData() {
+
+  // fetch("/gether/gatRegi.com", {
+  //   method: 'POST', // 요청 메서드 설정
+  //   headers: {
+  //     'Content-Type': 'application/json' // 요청 헤더 설정
+  //   },
+  //   body: JSON.stringify({}) // 요청 바디에 데이터 포함
+  // })
+  // .then(response => response.json())
+  // .then(data => {
+  //   // 서버에서 받아온 JSON 데이터를 사용
+  //   console.log(data);
+  //   // 받아온 데이터를 활용하여 추가적인 처리 수행
+  // })
+  // .catch(error => {
+  //   console.error('데이터를 받아오는 중 오류 발생:', error);
+  // });
+
   const regiData = document.getElementById('regi').value;
   const cleanedData = regiData.replace(/[[\]\ ]/g, '');
   const splitData = cleanedData.split("},{");
@@ -98,8 +116,9 @@ function controlRegiData() {
       regiList.push(map);
   });
 
-  
+  let parentCodeList = [];
   regiList.forEach(item => {
+    parentCodeList.push(item.PARENTS_CODE);
     const regiCode = item.REGI_CODE;
 
     // 부모 지역에 대한 로직
@@ -111,6 +130,11 @@ function controlRegiData() {
       createChildRegi(item, item.PARENTS_CODE);
     }
   });
+
+  let newParentCodeList = [...new Set(parentCodeList)];
+
+  showChildRegi(newParentCodeList);
+
 }
 
 
@@ -120,63 +144,92 @@ function createNode(item) {
   const parentRegiNode = document.createElement('div');
   parentRegiNode.className = 'R_1';
   parentRegiNode.innerHTML = item.REGI_NAME;
+  parentRegiNode.dataset.code = item.PARENTS_CODE;
 
   document.querySelector('.level1').appendChild(parentRegiNode); // 추가
 
-  // 자식 노드 생성 (checkbox들을 담을 div)
+  //자식 노드 생성 (checkbox들을 담을 div)
   const childRegiNode = document.createElement('div');
   childRegiNode.className = 'level2';
   childRegiNode.dataset.pcode = item.PARENTS_CODE;
 
-  parentRegiNode.appendChild(childRegiNode); // 추가
+  document.querySelector('.level1').appendChild(childRegiNode);
 }
 
 
 /* 자식 지역 체크박스 생성 함수 */
 function createChildRegi(item, pcode) {
   if(item.PARENTS_CODE == pcode) {
-    const childRegilabel = document.createElement('label');
-    childRegilabel.className = 'childlabel';
-    childRegilabel.innerHTML = item.REGI_NAME;
-
     const childRegi = document.createElement('input');
     childRegi.type = 'checkbox';
     childRegi.className = 'child';
+    childRegi.dataset.regiCode = item.REGI_CODE;
 
-    const targetElement = document.querySelector(`div[data-pcode=${pcode}]`)
+    /* 지역 선택시 동작 함수 */
+    let regiCheckArr = [];
+
+    // onchange 이벤트를 감지하여 처리하는 함수 정의
+    const handleChange = function(event) {
+      console.log(event.target)
+      const target = event.target;
+      const regiCode = target.getAttribute('data-regi-code');
+
+      regiCheckArr.push({ 'REGI_CODE': regiCode });
+
+    };
+
+    childRegi.addEventListener('change', handleChange);
+
+    const childRegilabel = document.createElement('label');
+    childRegilabel.className = 'childlabel';
+    childRegilabel.innerHTML = item.REGI_NAME;
     
-    targetElement.appendChild(childRegilabel);
+    const targetElement = document.querySelector(`div[data-pcode="${pcode}"]`);
     targetElement.appendChild(childRegi);
+    targetElement.appendChild(childRegilabel);
   }
 }
 
-function showChildRegi() {
-  const level1 = document.querySelectorAll('.R_1');
-  
-  level1.forEach((parent, i) => {
+/* 자식 지역 이벤트 리스너 */
+function showChildRegi(parentCodeList) {
+ 
+  parentCodeList.forEach((pcode, i) => {
 
-    let pCode = parent.getAttribute('data-code')
-
-    parent.addEventListener('mouseover', ()=> {
-        let childRegi = document.querySelectorAll(`div[data-pcode="${pCode}"]`);
-        const level2Container = document.querySelector('.level2');
-
-        childRegi.forEach(child => {
-          child.style.display = 'flex';
-        });
-    });
-  
-    parent.addEventListener('mouseleave', (e)=> {
-      let childRegi = document.querySelectorAll(`div[data-pcode="${pCode}"]`);
-      childRegi.forEach(child => {
-        child.style.display = 'none';
-      });
-    });
-
-    // let childRegi = document.querySelectorAll(`div[data-pcode="${pCode}"]`);
+    const targetElement = document.querySelector(`div[data-code="${pcode}"]`);
     
-    // for (let i = 0; i < childRegi.length; i++) {
-    //   childRegi[i].style.display = 'flex !important';
-    // }
+    targetElement.addEventListener('mouseover', (e)=>{
+      const childElement = e.target.nextElementSibling;
+      childElement.style.display = 'flex';
+    });
+
+    targetElement.addEventListener('mouseleave', (e)=>{
+      const childElement = e.target.nextElementSibling;
+      childElement.style.display = 'none';
+    });
+
   });
+
 }
+
+let regiCheckArr = [];
+function checkRegi(regi) {
+
+  const regiCode = regi.getAttribute('data-regi-code');
+  const isChecked = regi.checked;
+
+  if (isChecked) {
+
+    regiCheckArr.push({ 'REGI_CODE': regiCode });
+
+  } else {
+
+    for (let i = 0; i < regiCheckArr.length; i++) {
+      if (regiCheckArr[i]['REGI_CODE'] === regiCode) {
+        regiCheckArr.splice(i, 1);
+        break;
+      }
+    }
+  }
+}
+
+console.log(regiCheckArr)
